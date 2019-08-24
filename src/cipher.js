967ee8b68df5532f94eb56c828ccfd48512f8d90
conyzarate <cony.zarate@gmail.com>
2019-08-24 17:29:36 -0400

    version 0.1

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,35 @@
-window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+
     
-  },
-  decode: () => {
-    /* Acá va tu código */
-  }
-};
+window.cipher = {
+
+    encode: (texto, desplazamiento) => { let ascii, resultado, end ="";
+
+        for (let i=0; i < texto.length; i++){
+        
+        ascii= texto[i].charCodeAt(0)
+        resultado = (parseInt(ascii)- 65 + desplazamiento)% 26 +65; 
+        resultado = String.fromCharCode(resultado);
+        
+        end = resultado + end
+        
+      }
+        return end;
+   
+  }, 
+ 
+    decode: (texto, desplazamiento) => { let ascii, resultado, end ="";
+
+
+    for (let i=0; i < texto.length; i++){
+        
+      ascii= texto[i].charCodeAt(0)
+      resultado = (parseInt(ascii)- 65 - desplazamiento)% 26 +65; 
+      resultado = String.fromCharCode(resultado);
+      
+      end = resultado + end
+      
+    }
+      return end;
+ 
+}, 
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-/*Acá va tu código*/
+let desplazamiento, texto;
+
+// acá se ingresa el número de desplazamiento y lo manda a la consola.
+ 
+document.getElementById("$offset").addEventListener("click", () => {
+    desplazamiento = document.getElementById("offset").value
+     desplazamiento = parseInt(desplazamiento);
+     
+    });
+
+// acá decifra cuando se ingresa el click
+
+document.getElementById("cipher").addEventListener("click", () => {
+    texto= document.getElementById("texto").value.toUpperCase();
+        document.getElementById("result").innerHTML = window.cipher.encode(texto, desplazamiento);
+      
+    });
+
+    // acá descifra
+document.getElementById("dicipher").addEventListener("click", () => {
+       texto= document.getElementById("texto").value.toUpperCase();
+       document.getElementById("result").innerHTML = window.cipher.decode(texto, desplazamiento);
+       
+    });
+
+document.getElementById("$offset").addEventListener("click", () => {
+        document.getElementById("secondPage").style.display= "block";
+        document.getElementById("userOff").style.display="none";
+        
+});
+
+document.getElementById("home").addEventListener("click", () => {
+        document.getElementById("userOff").style.display="block";
+        document.getElementById("secondPage").style.display="none";
+
+});
+ 
+
